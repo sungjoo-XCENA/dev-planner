2b3eb869fc451cd682c8e6978289c5119f0b997d
sungjoo-XCENA <sungjoo.ha@xcena.com>
2026-04-27 23:50:26 +0900

    Add lineup types

---
+++ src/types/lineup.ts
@@ -0,0 +1,47 @@
+import type { PositionGroup } from "./player";
+import type { TeamName } from "./team";
+
+export type Quarter = 1 | 2 | 3 | 4;
+export type LineupRole = "FIELD" | "GK" | "BENCH";
+
+export type LineupSlot = {
+  quarter: Quarter;
+  team: TeamName;
+  playerId: string;
+  playerName: string;
+  assignedGroup?: PositionGroup;
+  role: LineupRole;
+  isDedicatedGk?: boolean;
+};
+
+export type TeamQuarterLineup = {
+  quarter: Quarter;
+  team: TeamName;
+  attack: string[];
+  mid: string[];
+  defense: string[];
+  gk: string;
+  bench: string[];
+  warnings: string[];
+};
+
+export type PlayerLineupSummary = {
+  playerId: string;
+  playerName: string;
+  team: TeamName;
+  assignedGroup: PositionGroup;
+  q1: LineupRole;
+  q2: LineupRole;
+  q3: LineupRole;
+  q4: LineupRole;
+  fieldCount: number;
+  gkCount: number;
+  benchCount: number;
+};
+
+export type LineupResult = {
+  quarters: TeamQuarterLineup[];
+  playerSummaries: PlayerLineupSummary[];
+  dedicatedGkRotation: Record<string, string[]>;
+  warnings: string[];
+};
